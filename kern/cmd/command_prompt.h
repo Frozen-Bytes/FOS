b5036350de61652069443c54672c57fcdffc7d95
KareemEssam7 <kareemessam2101@gmail.com>
2024-10-15 13:25:00 +0300

    feat: added implementation for 'process_command'
    added implementation for 'process_command' function by also creating 'is_subsequence' function to check if a string is a subsequence from another

--- kern/cmd/command_prompt.h
+++ kern/cmd/command_prompt.h
@@ -1,7 +1,7 @@
 #ifndef FOS_KERN_MONITOR_H
 #define FOS_KERN_MONITOR_H
 #ifndef FOS_KERNEL
-# error "This is a FOS kernel header; user programs should not #include it"
+#error "This is a FOS kernel header; user programs should not #include it"
 #endif
 #include <inc/stdio.h>
 #include <inc/string.h>
@@ -12,17 +12,18 @@
 #include <inc/error.h>
 
 /*2024*/
-//Structure for commands list
+// Structure for commands list
 typedef LIST_ENTRY(Command) Command_LIST_entry_t;
 LIST_HEAD(Command_LIST, Command);
 
-//List of found commands
+// List of found commands
 struct Command_LIST foundCommands;
 
-enum{
-CMD_INVALID = -3,
-CMD_INV_NUM_ARGS,
-CMD_MATCHED,
+enum
+{
+    CMD_INVALID = -3,
+    CMD_INV_NUM_ARGS,
+    CMD_MATCHED,
 };
 //================================================
 
@@ -32,7 +33,7 @@ void command_prompt_readline(const char *, char *);
 void get_into_prompt() __attribute__((noreturn));
 
 /*2024*/
-int process_command(int number_of_arguments, char** arguments);
+int process_command(int number_of_arguments, char **arguments);
+bool is_subsequence(const char *subsequence, const char *sequence);
 
-
-#endif	// !FOS_KERN_MONITOR_H
+#endif // !FOS_KERN_MONITOR_H
