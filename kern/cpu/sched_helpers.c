db9dd1d4149256c3c75d1359ebc8a2f4643128ee
zedan <abdom.zedan@gmail.com>
2024-12-08 22:33:40 +0200

    feat(sched_helpers): implement `env_set_priority`

--- kern/cpu/sched_helpers.c
+++ kern/cpu/sched_helpers.c
@@ -702,20 +702,30 @@ int get_load_average()
 void env_set_priority(int envID, int priority)
 {
 	//TODO: [PROJECT'24.MS3 - #06] [3] PRIORITY RR Scheduler - env_set_priority
+	//Your code is here
 
 	//Get the process of the given ID
 	struct Env* proc ;
 	envid2env(envID, &proc, 0);
 
-	//Your code is here
-	//Comment the following line
-	panic("Not implemented yet");
+	assert(proc);
+
+	if (proc->priority == priority) {
+		return;
+	}
+
+	if (proc->env_status == ENV_READY) {
+		sched_remove_ready(env);
+		proc->priority = priority;
+		sched_insert_ready(env);
+	} else {
+		proc->priority = priority;
+	}
 }
 
 void sched_set_starv_thresh(uint32 starvThresh)
 {
 	//TODO: [PROJECT'24.MS3 - #06] [3] PRIORITY RR Scheduler - sched_set_starv_thresh
 	//Your code is here
-	//Comment the following line
-	panic("Not implemented yet");
+	starvation_threshold = starvThresh;
 }
