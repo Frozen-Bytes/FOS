928980d1c5c543cc2519cf32a84c634d3ddeb879
zedan <abdom.zedan@gmail.com>
2024-12-26 17:35:52 +0200

    feat(stdio): add `cprintf_colored` function and link with corresponding system calls

--- inc/stdio.h
+++ inc/stdio.h
@@ -24,8 +24,33 @@ void	vprintfmt(void (*putch)(int, void*), void *putdat, const char *fmt, va_list
 
 // lib/printf.c
 int	cprintf(const char *fmt, ...);
+int cprintf_colored(int color, const char *fmt, ...);
 int	atomic_cprintf(const char *fmt, ...);
 int	vcprintf(const char *fmt, va_list);
+int	vcprintf_colored(const char *fmt, va_list);
+
+// defines for cprintf_colored
+
+// forground colors (text colors)
+enum
+{
+    TEXT_black = 0x0,
+    TEXT_blue,
+    TEXT_green,
+    TEXT_cyan,
+    TEXT_red,
+    TEXT_magenta,
+    TEXT_brown,
+    TEXT_light_grey,
+    TEXT_dark_grey,
+    TEXT_light_blue,
+    TEXT_light_green,
+    TEXT_light_cyan,
+    TEXT_light_red,
+    TEXT_light_magenta,
+    TEXT_yellow,
+    TEXT_white
+};
 
 // lib/sprintf.c
 int	snprintf(char *str, int size, const char *fmt, ...);
--- lib/printf.c
+++ lib/printf.c
@@ -10,6 +10,8 @@
 #include <inc/stdarg.h>
 #include <inc/lib.h>
 
+static int current_text_color = 0x0700;
+
 // Collect up to 256 characters into a buffer
 // and perform ONE system call to print all of them,
 // in order to make the lines output to the console atomic
@@ -50,6 +52,41 @@ int cprintf(const char *fmt, ...) {
 	printProgName = 1 ;
 	va_start(ap, fmt);
 	cnt = vcprintf(fmt, ap);
+	va_end(ap);
+
+	return cnt;
+}
+
+static void putch_colored(int ch, struct printbuf *b) {
+	b->buf[b->idx++] = ch;
+	if (b->idx == 256 - 1) {
+		sys_cputs_colored(b->buf, b->idx, printProgName, current_text_color);
+		b->idx = 0;
+	}
+	b->cnt++;
+}
+
+int	vcprintf_colored(const char *fmt, va_list ap)
+{
+	struct printbuf b;
+
+	b.idx = 0;
+	b.cnt = 0;
+	vprintfmt((void*) putch_colored, &b, fmt, ap);
+	sys_cputs_colored(b.buf, b.idx, printProgName, current_text_color);
+
+	printProgName = 0;
+	return b.cnt;
+}
+
+int cprintf_colored(int color, const char *fmt, ...)
+{
+	va_list ap;
+	int cnt;
+	printProgName = 1 ;
+	va_start(ap, fmt);
+	current_text_color = color << 8;
+	cnt = vcprintf_colored(fmt, ap);
 	va_end(ap);
 
 	return cnt;
